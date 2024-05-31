3442beb027ab58f7c35dc1c5af0fa3ee85d23672
Secret6230 <165053975+Secret6230@users.noreply.github.com>
2024-06-01 00:13:20 +0200

    Delete JS-HTML-CSS-StoffVonTechstarter/react/my-app/autoverkaufs-webseite/src directory

--- JS-HTML-CSS-StoffVonTechstarter/react/my-app/autoverkaufs-webseite/src/ha23-5-24/src/Timer.js
+++
@@ -1,9 +0,0 @@
-// const count = 15;
-// const timer = setInterval(function() {
-//   count--;
-//   console.log(count);
-//   if (count === 0) {
-//     clearInterval(timer);
-//     console.log("Countdown finished!");
-//   }
-// }, 1000);
